48daa20ddeaa0c7f01e8dbabbd407f0c03454550
Vignesh Kumar Rajasekaran <Vignesh@Vigneshs-MacBook-Pro.local>
2016-06-21 23:21:53 -0500

    added to repository

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Variables
+    var multipleToAdd = 0
+    var sum = 0
+    var val1 = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var multipleTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var sumLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    
+    @IBAction func onPlayBtnClicked(sender: AnyObject) {
+        
+        if multipleTxt.text != nil && multipleTxt.text != "" {
+            logo.hidden = true
+            multipleTxt.hidden = true
+            playBtn.hidden = true
+            
+            sumLbl.hidden = false
+            addBtn.hidden = false
+            
+            multipleToAdd = Int(multipleTxt.text!)!
+        }
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func onAddBtnClicked(sender: AnyObject) {
+        
+        sum = val1 + multipleToAdd
+        
+        sumLbl.text = "\(val1)"+" "+"+"+" " + "\(multipleToAdd)"+" "+"="+" "+"\(sum)"
+        
+        val1 = sum
+            
+        if isGameOver(){
+            restartGame()
+        }
+        
+    }
+    
+    
+    func restartGame(){
+        sum = 0
+        multipleToAdd = 0
+        val1 = 0
+        multipleTxt.text = ""
+        sumLbl.text = "Press ADD to add"
+        
+        logo.hidden = false
+        multipleTxt.hidden = false
+        playBtn.hidden = false
+        
+        sumLbl.hidden = true
+        addBtn.hidden = true
+        
+    }
+    
+    func isGameOver() -> Bool{
+        if sum >= 50{
+            return true
+        }
+        else{
+            return false
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
 }
 
